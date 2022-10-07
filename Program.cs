4095c778d490211a5b5ddc30ebef62e661a5db63
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-07 06:32:47 +0600

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter number of rows of array ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter number of columns of array ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = getArray(rows, cols);
+printArray(array);
+
+int[,] getArray(int m, int n) {
+    int[,] newArray = new int[m, n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            newArray[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return newArray;
+}
+
+void printArray (int[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
